011760aa30c17e7c682f52df231e503e3bedee4c
GeneralKenobi1 <43397938+GeneralKenobi1@users.noreply.github.com>
2020-06-26 11:14:46 -0700

    update project description

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 	</h1>
 	
 	<p>
-	This is a tool that you can copy/paste AdBlock filters (also known as Static Filter Syntax) into and get an explanation of what every piece of syntax does. Good for learning.
+	This is a tool into which you can copy/paste AdBlock filters (also known as Static Filter Syntax). It'll color code and it'll give you an explanation of what every piece of syntax does. Good for learning.
 	</p>
 	
 	<p>
@@ -35,18 +35,16 @@
 	
 	<p>
 	JSON:<br />
-	<!-- TODO: change this to HTML, color code each "syntax" differently, when hovering over each syntax with mouse display a tooltip with the info -->
 	<textarea id="json" disabled></textarea>
 	</p>
 	
 	<p>
 	HTML:<br />
-	<!-- TODO: change this to HTML, color code each "syntax" differently, when hovering over each syntax with mouse display a tooltip with the info -->
 	<textarea id="html" disabled></textarea>
 	</p>
 	
 	<p>
-	This project is is in progress. Want to <a href="https://github.com/GeneralKenobi1/adblock-validator">help with development</a>? Want to <a href="https://github.com/GeneralKenobi1/adblock-validator/issues">report a bug</a>? Visit our GitHub!
+	This project is in progress. <a href="https://github.com/GeneralKenobi1/adblock-validator/issues">Report bugs on GitHub.</a>
 	</p>
 	
 	<p>
@@ -56,10 +54,6 @@
 	<a href="https://github.com/gorhill/uBlock/wiki/Resources-Library">GitHub.com - uBlock Origin's JavaScript Scriptlet Syntax
 	<a href="https://adblockplus.org/en/filter-cheatsheet">AdBlockPlus.org - Filter Cheat Sheet</a><br />
 	<a href="https://kb.adguard.com/en/general/how-to-create-your-own-ad-filters">AdGuard.com - How to create your own ad filters</a>
-	</p>
-	
-	<p>
-	Note: There is more than one dialect of AdBlock Filter Syntax. For example, for JavaScript scripting, <a href="https://github.com/gorhill/uBlock">uBlock Origin</a> uses ##+js(), and <a href="https://adblockplus.org/">AdBlockPlus</a> uses #$#.
 	</p>
 </body>
 
