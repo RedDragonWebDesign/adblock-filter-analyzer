1847f9723f13a6afa7ab0764e8d524b9f38274b0
GeneralKenobi1 <43397938+GeneralKenobi1@users.noreply.github.com>
2020-07-09 00:57:53 -0700

    add tests, add options

--- FunctionLists/options.js
+++ FunctionLists/options.js
@@ -3,34 +3,45 @@
 
 "use strict";
 
+// https://help.eyeo.com/en/adblockplus/how-to-write-filters#options
+// https://kb.adguard.com/en/general/how-to-create-your-own-ad-filters#modifiers
+
 // [a-z0-9\-~]  no capital letters allowed
 export let optionsWithoutEquals = [
 	'1p',
 	'3p',
 	'all',
+	'app', // adguard
 	'badfilter',
 	'cname',
+	'content', // adguard
+	'cookie', // adguard
 	'csp', // allowed both without equals and with equals
 	'css',
 	'doc',
 	'document',
+	'domain', // adguard
 	'ehide',
 	'elemhide',
 	'empty',
+	'extension', // adguard
 	'first-party',
 	'font',
 	'frame',
-	// 'genericblock', // not supported by ubo
+	'genericblock', // adguard
 	'generichide',
 	'ghide',
 	'image',
 	'important',
 	'inline-font',
 	'inline-script',
+	'jsinject', // adguard
 	'match-case',
 	'media',
 	'mp4',
+	'network', // adguard
 	'object',
+	'object-subrequest', // adguard
 	'other',
 	'ping',
 	'popunder',
@@ -38,9 +49,11 @@ export let optionsWithoutEquals = [
 	'script',
 	'shide',
 	'specifichide',
+	'stealth', // adguard
 	'stylesheet',
 	'subdocument',
 	'third-party',
+	'urlblock', // adguard
 	'webrtc',
 	'websocket',
 	'xhr',
@@ -53,6 +66,8 @@ export let optionsWithEquals = [
 	'domain', // = [~|a-z.]
 	'redirect', // = [a-z0-9\-./]
 	'redirect-rule', // = [a-z0-9\-./]
+	'removeparam', // adguard
+	'replace', // adguard
 	'rewrite', // = [a-z\-:]
 	'sitekey', // = [a-z]
 ];
