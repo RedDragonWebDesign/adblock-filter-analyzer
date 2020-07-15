6b81e6257c2f47db181da154f5866cfa80178e0e
GeneralKenobi1 <43397938+GeneralKenobi1@users.noreply.github.com>
2020-07-14 18:47:48 -0700

    fix #44 add a dropdown list that loads external files, including my tests and major lists

--- index.php
+++ index.php
@@ -19,7 +19,73 @@
 	</p>
 	
 	<p>
-	<button id="clear">Clear</button> LineCount: <span id="line-count"></span>
+	<button id="clear">Clear</button>
+	<select id="filter-list">
+		<option value="./tests/test-good-filters.txt">
+			Test List 1 - Should Always Pass (No Errors) (373)</option>
+		<option value="./tests/test-bad-filters.txt">
+			Test List 2 - Should Always Fail (Errors) (137)</option>
+		<option value="https://cdn.jsdelivr.net/gh/uBlockOrigin/uAssets@master/filters/filters.txt">
+			uBlock filters (23,503)</option>
+		<option value="https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt">
+			uBlock filters - Badware risks (860)</option>
+		<option value="https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt">
+			uBlock filters - Privacy (207)</option>
+		<option value="https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt">
+			uBlock filters - Resource abuse (117)</option>
+		<option value="https://gitcdn.xyz/repo/uBlockOrigin/uAssets/master/filters/unbreak.txt">
+			uBlock filters - Unbreak (1,463)</option>
+		<option value="https://filters.adtidy.org/extension/ublock/filters/2_without_easylist.txt">
+			AdGuard Base (37,577)</option>
+		<option value="https://filters.adtidy.org/extension/ublock/filters/11.txt">
+			AdGuard Mobile Ads</option>
+		<option value="https://easylist.to/easylist/easylist.txt">
+			EasyList (84,037)</option>
+		<option value="https://filters.adtidy.org/extension/ublock/filters/3.txt">
+			AdGuard Tracking Protection</option>
+		<option value="https://easylist.to/easylist/easyprivacy.txt">
+			EasyPrivacy (17,912)</option>
+		<option value="https://www.fanboy.co.nz/enhancedstats.txt">
+			Fanboy's Enhanced Tracking List</option>
+		<option value="https://glcdn.githack.com/curben/urlhaus-filter/raw/master/urlhaus-filter-online.txt">
+			Online Malicious URL Blocklist (5,586)</option>
+		<option value="https://raw.githubusercontent.com/Spam404/lists/master/adblock-list.txt">
+			Spam404</option>
+		<option value="https://filters.adtidy.org/extension/ublock/filters/14.txt">
+			AdGuard Annoyances (30,038)</option>
+		<option value="https://filters.adtidy.org/extension/ublock/filters/4.txt">
+			AdGuard Social Media (11,057)</option>
+		<option value="https://fanboy.co.nz/fanboy-antifacebook.txt">
+			Anti-Facebook (68)</option>
+		<option value="https://easylist-downloads.adblockplus.org/easylist-cookie.txt">
+			EasyList Cookie (17,339)</option>
+		<option value="https://easylist.to/easylist/fanboy-annoyance.txt">
+			Fanboy's Annoyance (55,065)</option>
+		<option value="https://easylist.to/easylist/fanboy-social.txt">
+			Fanboy's Social (23,862)</option>
+		<option value="https://gitcdn.xyz/repo/uBlockOrigin/uAssets/master/filters/annoyances.txt">
+			uBlock filters - Annoyances (3,468)</option>
+		<option value="https://someonewhocares.org/hosts/hosts">
+			Dan Pollock's hosts file</option>
+		<option value="https://winhelp2002.mvps.org/hosts.txt">
+			MVPS HOSTS</option>
+		<option value="https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext">
+			Peter Lowe's Ad and tracking server list</option>
+		<option value="https://easylist-downloads.adblockplus.org/Liste_AR.txt">
+			ara: Liste AR</option>
+		<option value="https://easylist-downloads.adblockplus.org/easylistchina.txt">
+			CHN: EasyList China (中文)</option>
+		<option value="https://raw.githubusercontent.com/easylist/EasyListHebrew/master/EasyListHebrew.txt">
+			ISR: EasyList Hebrew</option>
+		<option value="https://raw.githubusercontent.com/yous/YousList/master/youslist.txt">
+			KOR: YousList</option>
+		<option value="https://easylist-downloads.adblockplus.org/advblock+cssfixes.txt">
+			RUS: RU AdList</option>
+		<option value="https://raw.githubusercontent.com/yourduskquibbles/webannoyances/master/ultralist.txt">
+			Web Annoyances Ultralist (33,634)</option>
+	</select>
+	LineCount: <span id="line-count"></span>
+	ProcessTime: <span id="timer"></span>
 	</p>
 	
 	<div id="flex-container">
