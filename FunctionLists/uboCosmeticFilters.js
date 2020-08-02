d36aa1c4f7e313b6c8af81f8e3eab72370e02796
GeneralKenobi1 <admiraladama99@gmail.com>
2020-08-02 05:53:36 -0700

    start cosmetic filters list

---
+++ FunctionLists/uboCosmeticFilters.js
@@ -0,0 +1,25 @@
+// Copyright https://www.RedDragonWebDesign.com/
+// Permission required to use or copy code. All rights reserved.
+
+"use strict";
+
+// https://github.com/gorhill/uBlock/wiki/Procedural-cosmetic-filters
+// https://github.com/gorhill/uBlock/wiki/Inline-script-tag-filtering
+export let uboCosmeticFilters = [
+	':has(',
+	':has-text(',
+	':matches-css(',
+	':matches-css-before(',
+	':matches-css-after(',
+	':min-text-length(',
+	':not(',
+	':upward(',
+	':watch-attr(',
+	':xpath(',
+	// deprecated
+	':contains(', // use ##^script:has-text() instead
+	':if(',
+	':if-not(',
+	':nth-ancestor(',
+	':watch-attrs(',
+];
