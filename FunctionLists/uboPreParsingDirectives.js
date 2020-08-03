b66a4babee8ffa568f135951efaac2040b2f4a83
GeneralKenobi1 <43397938+GeneralKenobi1@users.noreply.github.com>
2020-08-03 07:41:52 -0700

    add ubo pre parsing directive function list

---
+++ FunctionLists/uboPreParsingDirectives.js
@@ -0,0 +1,29 @@
+// Copyright https://www.RedDragonWebDesign.com/
+// Permission required to use or copy code. All rights reserved.
+
+"use strict";
+
+// https://github.com/gorhill/uBlock/wiki/Procedural-cosmetic-filters
+export let uboPreParsingDirectives = [
+	'endif',
+	'if',
+	'include',
+];
+
+export let uboPreParsingIfConditions = [
+	'adguard',
+	'adguard_app_windows',
+	'adguard_ext_chromium',
+	'adguard_ext_edge',
+	'adguard_ext_firefox',
+	'adguard_ext_opera',
+	'cap_html_filtering',
+	'cap_user_stylesheet',
+	'env_chromium',
+	'env_edge',
+	'env_firefox',
+	'env_mobile',
+	'env_safari',
+	'ext_ublock',
+	'false',
+];
